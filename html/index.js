2a153c73ed01a45060f55131e03273937512cc2d
Sean <sareid@outlook.com>
2019-05-21 16:41:25 -0400

    Form changes to avoid spam

---
+++ html/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const request = require("request");
+
+app.use(bodyParser.json());
+
+//Uncomment for local testing
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+const formUrl =
+  "https://docs.google.com/a/reidrepairs.com/forms/d/1qdhCt3fDuoNEPx3uhPz4wSh8TL3mUCzcgcm6BQWjRTg/formResponse";
+
+app.post("/form/", function(req, res) {
+  const testValueCheck = req.body.find(item => item.name === "testAnswer")
+    .value;
+  if (testValueCheck != "2" || testValueCheck !== "two") {
+    res.send("Ok");
+    return;
+  }
+  const googleString = req.body.reduce((str, item, idx) => {
+    const char = idx === 0 ? "?" : "&";
+    return (str +=
+      item.name === "testAnswer" ? "" : `${char}${item.name}=${item.value}`);
+  }, "");
+
+  const requestUrl = `${formUrl}${googleString}`;
+  request.post(requestUrl, null, (error, r, body) => {
+    if (error) {
+      res.send(error);
+      return;
+    }
+    res.send(r);
+  });
+});
+
+app.listen(3000);
