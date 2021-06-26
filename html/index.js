3fcd72f894ed10625ea885547159eb5ba0740579
Sean <sareid@outlook.com>
2021-06-26 08:10:43 -0400

    CORS

--- html/index.js
+++ html/index.js
@@ -10,6 +10,7 @@ app.use("*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://reidrepairs.com*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Accept");
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
